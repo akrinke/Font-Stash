99026dedd65e0f38dac11e26835e5d5e997368de
r-lyeh <rlyeh.nospam@gmail.com>
2012-11-07 10:32:19 +0100

    Update fontstash.h

--- fontstash.h
+++ fontstash.h
@@ -26,7 +26,7 @@ int sth_add_font(struct sth_stash* stash, const char* path);
 int sth_add_font_from_memory(struct sth_stash* stash, unsigned char* buffer);
 
 int  sth_add_bitmap_font(struct sth_stash* stash, int ascent, int descent, int line_gap);
-void sth_add_glyph(struct sth_stash* stash, int idx, GLuint id, const char* s,  // @rlyeh
+void sth_add_glyph(struct sth_stash* stash, int idx, GLuint id, const char* s,  /* @rlyeh: function does not return int */
                   short size, short base, int x, int y, int w, int h,
                   float xoffset, float yoffset, float xadvance);
 
